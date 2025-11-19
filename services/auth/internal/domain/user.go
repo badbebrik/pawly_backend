eeb6029874b100f95c5d14412815a9f70d2fdcf7
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2025-11-19 19:50:53 +0300

    add user entity

---
+++ services/auth/internal/domain/user.go
@@ -0,0 +1,22 @@
+package domain
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type User struct {
+	ID           uuid.UUID
+	Email        string
+	PasswordHash *string
+	IsVerified   bool
+	IsActive     bool
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	LastLoggedAt time.Time
+	DeletedAt    *time.Time
+}
+
+func (u *User) HasPassword() bool {
+	return u.PasswordHash != nil && *u.PasswordHash != ""
+}

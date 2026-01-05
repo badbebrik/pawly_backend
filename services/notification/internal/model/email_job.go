97b30f30221347c7c6a2de7afdcdbbedd8eef094
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2026-01-05 16:38:17 +0300

    add model

--- services/notification/internal/model/email_job.go
+++ services/notification/internal/model/email_job.go
@@ -1,1 +1,10 @@
 package model
+
+type EmailJob struct {
+	Email    string         `json:"email"`
+	Template string         `json:"template"`
+	Locale   string         `json:"locale"`
+	Subject  string         `json:"subject"`
+	Data     map[string]any `json:"data"`
+	Meta     map[string]any `json:"meta"`
+}

ba76b7b8af4b1f79c3f93a9b93a9430bbac34b94
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2026-01-07 00:28:56 +0300

    add app configuration and running to main

--- services/profile/cmd/main.go
+++ services/profile/cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"os"
+	"profile/internal/app"
 	"profile/internal/config"
 )
 
@@ -14,4 +15,14 @@ func main() {
 
 	cfg := config.Load()
 	_ = cfg
+
+	a, err := app.New(cfg)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to init app")
+	}
+	defer a.Close()
+
+	if err := a.Run(); err != nil {
+		log.Fatal().Err(err).Msg("application stopped with error")
+	}
 }

c116690f9da213ca418ced79c15eac8e614a78b0
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2026-01-13 23:37:34 +0300

    add dtos for color

---
+++ services/catalog/internal/transport/http/dto/color.go
@@ -0,0 +1,23 @@
+package dto
+
+type ColorItem struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Hex      string `json:"hex"`
+	IsActive bool   `json:"is_active"`
+	Version  int    `json:"version"`
+}
+
+type CreateColorRequest struct {
+	NameRu   string `json:"name_ru"`
+	NameEn   string `json:"name_en"`
+	Hex      string `json:"hex"`
+	IsActive *bool  `json:"is_active"`
+}
+
+type UpdateColorRequest struct {
+	NameRu   *string `json:"name_ru"`
+	NameEn   *string `json:"name_en"`
+	Hex      *string `json:"hex"`
+	IsActive *bool   `json:"is_active"`
+}

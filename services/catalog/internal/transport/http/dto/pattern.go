f180e20af9b8a968725fb61dce0b63c722e625c0
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2026-01-13 23:38:14 +0300

    add dtos for patterns

---
+++ services/catalog/internal/transport/http/dto/pattern.go
@@ -0,0 +1,23 @@
+package dto
+
+type PatternItem struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	IconKey  string `json:"icon_key"`
+	IsActive bool   `json:"is_active"`
+	Version  int    `json:"version"`
+}
+
+type CreatePatternRequest struct {
+	NameRu   string `json:"name_ru"`
+	NameEn   string `json:"name_en"`
+	IconKey  string `json:"icon_key"`
+	IsActive *bool  `json:"is_active"`
+}
+
+type UpdatePatternRequest struct {
+	NameRu   *string `json:"name_ru"`
+	NameEn   *string `json:"name_en"`
+	IconKey  *string `json:"icon_key"`
+	IsActive *bool   `json:"is_active"`
+}

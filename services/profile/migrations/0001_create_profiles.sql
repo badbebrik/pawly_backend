48ad9705bc56d07a84fe09874f389df8ee91255d
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2026-01-23 01:19:31 +0300

    change db profile schema

--- services/profile/migrations/0001_create_profiles.sql
+++ services/profile/migrations/0001_create_profiles.sql
@@ -1,13 +1,19 @@
+-- +goose Up
 CREATE TABLE profiles (
     user_id       UUID PRIMARY KEY,
     first_name    TEXT,
     last_name     TEXT,
-    avatar_url    TEXT,
+    avatar_file_id UUID NULL,
+    notification_settings JSONB NOT NULL DEFAULT '{}'::jsonb,
+    public_contact_settings JSONB NOT NULL DEFAULT '{}'::jsonb,
+    extra_contacts JSONB NOT NULL DEFAULT '{}'::jsonb,
     phone         TEXT,
     locale        VARCHAR(8)  NOT NULL DEFAULT 'ru',
     timezone      VARCHAR(64) NOT NULL DEFAULT 'Europe/Moscow',
     date_format   VARCHAR(32) NOT NULL DEFAULT 'dd.MM.yyyy',
-    notifications JSONB       NOT NULL DEFAULT '{}'::jsonb,
     created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at    TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+
+-- +goose Down
+DROP TABLE IF EXISTS profiles;

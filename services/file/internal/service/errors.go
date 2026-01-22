eaf5d78a9ca2298b3610d396ef84335428fa7c5e
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2026-01-22 04:09:14 +0300

    fix typo

--- services/file/internal/service/errors.go
+++ services/file/internal/service/errors.go
@@ -6,7 +6,7 @@ import (
 
 var (
 	ErrInvalidInput  = errors.New("invalid_input")
-	ErrInvalidState  = erorrs.New("invalid_state")
+	ErrInvalidState  = errors.New("invalid_state")
 	ErrUploadExpired = errors.New("upload_expired")
 	ErrNotReady      = errors.New("not_ready")
 )
